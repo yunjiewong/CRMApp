83314013105f9a63dcc0cbdbd997fa78f62ae56c
Yunjiewong <aaronwongyj@gmail.com>
2022-06-10 13:11:03 -0400

    change

--- Antra.CRMApp.WebMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Antra.CRMApp.WebMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
